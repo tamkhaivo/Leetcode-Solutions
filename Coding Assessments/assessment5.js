ff5f610beddf23ce18c0990cdb789c0bbfe3fe08
i-got-to-eat <khaitamvo@gmail.com>
2024-12-03 15:48:35 -0800

    Assessment 5

---
+++ Coding Assessments/assessment5.js
@@ -0,0 +1,35 @@
+/*
+    Assessment 2024: 52 68 6F 6D 62 75 73 0D 0A
+*/
+
+module.exports = {
+    isSmoothArray: isSmoothArray
+};
+
+/*
+
+    find the min trips to deliver n packets of varying sizes
+    n = 3
+    packets = [1.01, 1.99, 2.5]
+    result = 2
+
+*/
+
+function minTrips(packetSize, packets) {
+    
+}
+/*
+
+    compare captions between video1 and video2 see if a substring appears in both captions.
+    return a bool to see if theres a substring
+    
+    videoComparisons = 3
+    video1 = ["ab", "bc", "dc"]
+    video2 = ["ex", "ee", "ce"]
+    result = "NO, NO, YES"
+
+*/
+function substringPresent(videoComparisons, video1, video2) {
+    
+    
+}
