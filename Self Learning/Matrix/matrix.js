f7e3e1fa0bffa021711db795efcc87a639467b92
i-got-to-eat <khaitamvo@gmail.com>
2024-12-24 23:03:58 -0800

    New Traversals

--- Self Learning/Matrix/matrix.js
+++ Self Learning/Matrix/matrix.js
@@ -1,8 +1,20 @@
 let matrix2D = [
   [1, 2],
-    [3, 4],
-    [5, 6]
+  [3, 4],
+  [5, 6],
+];
 
+let chess = [
+  ["P", "E", "E", "P"],
+  ["E", "P", "E", "P"],
+  ["P", "E", "P", "P"],
+  ["P", "E", "P", "E"],
+];
+let board = [
+    ['E', 'P', 'E', 'P'],
+    ['P', 'E', 'P', 'E'],
+    ['E', 'P', 'E', 'P'],
+    ['P', 'E', 'P', 'E']
 ];
 function deleteColumn(matrix, column) {
   for (let row = 0; row < matrix.length; row++) {
@@ -73,30 +85,111 @@ function transpose(matrix) {
 }
 
 function reverseTranspose(matrix) {
-    const rows = matrix.length;
-    const cols = rows > 0 ? matrix[0].length : 0;
-    let output = [];
+  const rows = matrix.length;
+  const cols = rows > 0 ? matrix[0].length : 0;
+  let output = [];
 
-    for (let row = rows - 1; row >= 0; row--){
-        output[cols - row - 1] = [];
-        for(let col = cols - 1; col >= 0; col--){
-            output[cols - row - 1][col] = matrix[col][row];
-        }
+  for (let row = rows - 1; row >= 0; row--) {
+    output[cols - row - 1] = [];
+    for (let col = cols - 1; col >= 0; col--) {
+      output[cols - row - 1][col] = matrix[col][row];
     }
-    return output;
+  }
+  return output;
 }
 
 function reflectOverSecondaryDiagonal(matrix) {
-    // Must be a square matrix 
-    let size = matrix.length;
-    let output = [];
-    for (let row = 0; row < size; row++){
-        output[row] = [];
-        for (let col = 0; col < size; col++){
-            output[row][col] = matrix[size - col - 1][size - row - 1];
+  // Must be a square matrix
+  let size = matrix.length;
+  let output = [];
+  for (let row = 0; row < size; row++) {
+    output[row] = [];
+    for (let col = 0; col < size; col++) {
+      output[row][col] = matrix[size - col - 1][size - row - 1];
+    }
+  }
+  return output;
+}
+
+function findAdjacentEmptyCells(matrix) {
+  const rows = matrix.length;
+  const cols = rows > 0 ? matrix[0].length : 0;
+  let output = [];
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (matrix[row][col] == "E") {
+        if (
+          (row > 0 && matrix[row - 1][col] == "E") ||
+          (row < rows - 1 && matrix[row + 1][col] == "E") ||
+          (col > 0 && matrix[row][col - 1] == "E") ||
+          (col < cols - 1 && matrix[row][col + 1] == "E")
+        ) {
+          output.push([row, col]);
+        }
+      }
+    }
+  }
+
+  return output;
+}
+
+function countSubmatrixCorners(matrix, subMatrixSize, char) {
+    const rows = matrix.length;
+    const cols = rows > 0 ? matrix[0].length : 0;
+    let count = 0;
+    for (let row = 0; row < rows - subMatrixSize + 1; row++){
+        for (let col = 0; col < cols - subMatrixSize + 1; col++) {
+            if (matrix[row][col] == char &&
+                matrix[row][col + subMatrixSize - 1] == char &&
+                matrix[row + subMatrixSize - 1][col] == char &&
+                matrix[row + subMatrixSize - 1][col + subMatrixSize - 1] == char
+            ) {
+                count++
+            }
         }
     }
-    return output;
+    return count;
 }
-console.table(matrix2D)
-console.table(reflectOverSecondaryDiagonal(matrix2D))
+
+function traverseLargesetPath(matrix, startRow, startCol) {
+    const rows = matrix.length;
+    const cols = rows > 0 ? matrix[0].length : 0;
+    const directions = [
+        [1, 0],
+        [-1,0],
+        [0,1],
+        [0,-1]
+    ]
+    let currentMax = matrix[startRow][startCol];
+    const path = [currentMax]
+    let row = startRow;
+    let col = startCol;
+    while (true) {
+        let newRow = -1;
+        let newCol = -1;
+
+        for (const direction of directions) {
+            row = row + direction[0];
+            col = col + direction[1];
+            if (row >= 0 &&
+                row < rows &&
+                col >= 0 &&
+                col < cols &&
+                matrix[row][col] > currentMax) {
+                    newRow = row;
+                    newCol = col;
+                    currentMax = matrix[row][col];
+                }
+        }
+        if (newRow == -1 && newCol == -1) {
+            break;
+        }
+        path.push(currentMax);
+        row = newRow;
+        col = newCol;
+    }
+    return path;
+}
+
+console.log(traverseLargesetPath(matrix2D, 0, 0))
