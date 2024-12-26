f1d7ceed37b261a39e060bec8d22a2f1ca1ee96a
i-got-to-eat <khaitamvo@gmail.com>
2024-12-25 19:29:51 -0800

    Update matrix.js

--- Self Learning/Matrix/matrix.js
+++ Self Learning/Matrix/matrix.js
@@ -11,10 +11,10 @@ let chess = [
   ["P", "E", "P", "E"],
 ];
 let board = [
-    ['E', 'P', 'E', 'P'],
-    ['P', 'E', 'P', 'E'],
-    ['E', 'P', 'E', 'P'],
-    ['P', 'E', 'P', 'E']
+  ["E", "P", "E", "P"],
+  ["P", "E", "P", "E"],
+  ["E", "P", "E", "P"],
+  ["P", "E", "P", "E"],
 ];
 function deleteColumn(matrix, column) {
   for (let row = 0; row < matrix.length; row++) {
@@ -135,61 +135,189 @@ function findAdjacentEmptyCells(matrix) {
 }
 
 function countSubmatrixCorners(matrix, subMatrixSize, char) {
-    const rows = matrix.length;
-    const cols = rows > 0 ? matrix[0].length : 0;
-    let count = 0;
-    for (let row = 0; row < rows - subMatrixSize + 1; row++){
-        for (let col = 0; col < cols - subMatrixSize + 1; col++) {
-            if (matrix[row][col] == char &&
-                matrix[row][col + subMatrixSize - 1] == char &&
-                matrix[row + subMatrixSize - 1][col] == char &&
-                matrix[row + subMatrixSize - 1][col + subMatrixSize - 1] == char
-            ) {
-                count++
-            }
-        }
+  const rows = matrix.length;
+  const cols = rows > 0 ? matrix[0].length : 0;
+  let count = 0;
+  for (let row = 0; row < rows - subMatrixSize + 1; row++) {
+    for (let col = 0; col < cols - subMatrixSize + 1; col++) {
+      if (
+        matrix[row][col] == char &&
+        matrix[row][col + subMatrixSize - 1] == char &&
+        matrix[row + subMatrixSize - 1][col] == char &&
+        matrix[row + subMatrixSize - 1][col + subMatrixSize - 1] == char
+      ) {
+        count++;
+      }
     }
-    return count;
+  }
+  return count;
 }
 
 function traverseLargesetPath(matrix, startRow, startCol) {
-    const rows = matrix.length;
-    const cols = rows > 0 ? matrix[0].length : 0;
-    const directions = [
-        [1, 0],
-        [-1,0],
-        [0,1],
-        [0,-1]
-    ]
-    let currentMax = matrix[startRow][startCol];
-    const path = [currentMax]
-    let row = startRow;
-    let col = startCol;
-    while (true) {
-        let newRow = -1;
-        let newCol = -1;
+  const rows = matrix.length;
+  const cols = rows > 0 ? matrix[0].length : 0;
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+  let currentMax = matrix[startRow][startCol];
+  const path = [currentMax];
+  let row = startRow;
+  let col = startCol;
+  while (true) {
+    let newRow = -1;
+    let newCol = -1;
 
-        for (const direction of directions) {
-            row = row + direction[0];
-            col = col + direction[1];
-            if (row >= 0 &&
-                row < rows &&
-                col >= 0 &&
-                col < cols &&
-                matrix[row][col] > currentMax) {
-                    newRow = row;
-                    newCol = col;
-                    currentMax = matrix[row][col];
-                }
-        }
-        if (newRow == -1 && newCol == -1) {
-            break;
-        }
-        path.push(currentMax);
-        row = newRow;
-        col = newCol;
+    for (const direction of directions) {
+      row = row + direction[0];
+      col = col + direction[1];
+      if (
+        row >= 0 &&
+        row < rows &&
+        col >= 0 &&
+        col < cols &&
+        matrix[row][col] > currentMax
+      ) {
+        newRow = row;
+        newCol = col;
+        currentMax = matrix[row][col];
+      }
     }
-    return path;
+    if (newRow == -1 && newCol == -1) {
+      break;
+    }
+    path.push(currentMax);
+    row = newRow;
+    col = newCol;
+  }
+  return path;
+}
+function findNegativeNumbersDiagonalTraversal() {
+  const matrix = [
+    [1, -2, 3, -4],
+    [5, -6, 7, 8],
+    [-9, 10, -11, 12],
+  ];
+
+  const rows = matrix.length;
+  const cols = rows > 0 ? matrix[0].length : 0;
+  const solution = [];
+  const traversal = [];
+  let row = 0;
+  let col = 0;
+  let traverseUp = true;
+  while (traversal.length < rows * cols) {
+    traversal[traversal.length] = matrix[row][col];
+    if (matrix[row][col] < 0) {
+      solution.push([row + 1, col + 1]);
+    }
+    if (traverseUp) {
+      if (row - 1 < 0) {
+        traverseUp = !traverseUp;
+        col++;
+      } else if (col + 1 >= cols) {
+        traverseUp = !traverseUp;
+        row++;
+      } else {
+        col++;
+        row--;
+      }
+    } else {
+      if (col - 1 < 0) {
+        traverseUp = !traverseUp;
+        row++;
+      } else if (row + 1 >= rows) {
+        traverseUp = !traverseUp;
+        col++;
+      } else {
+        col--;
+        row++;
+      }
+    }
+  }
+  console.log(traversal);
+  console.log(solution); // Output: [[1, 2], [3, 1], [2, 2], [1, 4], [3, 3]]
 }
 
-console.log(traverseLargesetPath(matrix2D, 0, 0))
+function traverseClockwiseSprialPath() {
+  const matrix = [
+    ["a", "b", "c", "d"],
+    ["e", "f", "g", "h"],
+    ["i", "j", "k", "l"],
+  ];
+
+  const rows = matrix.length;
+  const cols = rows > 0 ? matrix[0].length : 0;
+  const traverse = [];
+  const solution = [];
+  let row = 0;
+  let col = 0;
+
+  const visited = new Array(rows)
+    .fill(null)
+    .map(() => new Array(cols).fill(false));
+
+  const Direction = {
+    UP: "UP",
+    DOWN: "DOWN",
+    LEFT: "LEFT",
+    RIGHT: "RIGHT",
+  };
+  let movement = Direction.RIGHT;
+
+  while (traverse.length < rows * cols) {
+    traverse[traverse.length] = matrix[row][col];
+    visited[row][col] = true;
+
+    switch (matrix[row][col]) {
+      case "a":
+      case "e":
+      case "i":
+      case "o":
+      case "u":
+        solution.push(traverse.length);
+        break;
+      default:
+        break;
+    }
+
+    switch (movement) {
+      case Direction.RIGHT:
+        if (col + 1 >= cols || visited[row][col + 1]) {
+          movement = Direction.DOWN;
+          row++;
+        } else {
+          col++;
+        }
+        break;
+      case Direction.LEFT:
+        if (col - 1 < 0 || visited[row][col - 1]) {
+          movement = Direction.UP;
+          row--;
+        } else {
+          col--;
+        }
+        break;
+      case Direction.DOWN:
+        if (row + 1 >= rows || visited[row + 1][col]) {
+          movement = Direction.LEFT;
+          col--;
+        } else {
+          row++;
+        }
+        break;
+      case Direction.UP:
+        if (row - 1 < 0 || visited[row - 1][col]) {
+          movement = Direction.RIGHT;
+          col++;
+        } else {
+          row--;
+        }
+        break;
+    }
+  }
+  console.log(traverse);
+  console.log(solution); //[1, 9, 10]
+}
